8ae66b37722ee587c5a502f521f15e38309bed9f
Jeong Park <wjdtn1220@gmail.com>
2019-08-16 01:34:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,8 @@
 def oxford_comma(array)
-  return array.first if array.size == 1
-  
   if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
+   return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    array[-1].insert(0,"and ")
   end
     array.join(", ")
 end
