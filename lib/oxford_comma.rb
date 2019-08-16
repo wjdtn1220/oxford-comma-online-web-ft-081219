0db966558f45429d4dda4a916c95d515964306fa
Jeong Park <wjdtn1220@gmail.com>
2019-08-16 02:52:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   if array.length == 2
-   return "#{array[0]} and #{array[1]}"
+    return "#{array[0]} and #{array[1]}"
   elsif array.length > 2
-    array[-1].insert(0,"and ")
+    array[-1].insert(0, "and ")
   end
-    array.join(", ")
+  array.join(", ")
 end
